b3e3a71692d9e7383ab1ab38ebc0b1f7396f6b09
Prakash <t.prakash1999@gmail.com>
2025-07-23 16:50:27 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,7 +6,6 @@ from sklearn.metrics import accuracy_score
 import mlflow
 import pickle
 
-
 # Load data
 df = pd.read_csv("data/iris.csv")
 X = df.drop("species", axis=1)
@@ -17,14 +16,12 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     test_size=0.2,
                                                     random_state=42)
 
-
 # Start MLflow experiment
 mlflow.set_experiment("Iris_Classifier")
 with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
     preds = model.predict(X_test)
-
     acc = accuracy_score(y_test, preds)
     mlflow.log_param("n_estimators", 100)
     mlflow.log_metric("accuracy", acc)
