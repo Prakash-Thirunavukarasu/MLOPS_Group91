acc3174d42fd9fc1a0bbb71fe742c740556fabd4
Prakash <t.prakash1999@gmail.com>
2025-07-23 16:48:54 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,12 +6,17 @@ from sklearn.metrics import accuracy_score
 import mlflow
 import pickle
 
+
 # Load data
 df = pd.read_csv("data/iris.csv")
 X = df.drop("species", axis=1)
 y = df["species"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2,
+                                                    random_state=42)
+
 
 # Start MLflow experiment
 mlflow.set_experiment("Iris_Classifier")
@@ -23,7 +28,6 @@ with mlflow.start_run():
     acc = accuracy_score(y_test, preds)
     mlflow.log_param("n_estimators", 100)
     mlflow.log_metric("accuracy", acc)
-
     # Save model
     with open("src/model.pkl", "wb") as f:
         pickle.dump(model, f)
