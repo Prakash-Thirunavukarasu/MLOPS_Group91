199639fab0c19896f11046f358e5e8d171e9abc2
Prakash <t.prakash1999@gmail.com>
2025-07-24 16:53:50 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,25 +3,31 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import logging
-from prometheus_fastapi_instrumentator import Instrumentator  # NEW
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 # Logging
 logging.basicConfig(filename="logs/prediction.log", level=logging.INFO)
+
 
 # Load model
 with open("src/model.pkl", "rb") as f:
     model = pickle.load(f)
 
+
 app = FastAPI()
+
 
 # Prometheus metrics instrumentator
 Instrumentator().instrument(app).expose(app)  # NEW
+
 
 class IrisInput(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
+
 
 @app.post("/predict")
 def predict(input: IrisInput):
